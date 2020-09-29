23355d818059c9c6b2c199f6b728d5af05ce9c0f
Johanna Fericean <71076130+johakespeare@users.noreply.github.com>
2020-09-29 17:55:03 +0200

    Update DeMemoire.java

--- atelier2/src/DeMemoire.java
+++ atelier2/src/DeMemoire.java
@@ -1,27 +1,35 @@
-public class DeMemoire extends De{
-private int lancer_precedent;
+/**
+ * Classe DeMemoire
+ * classe fille de la classe De
+ * @version 1.0
+ *
+ * @author Johanna Fericean
+ */
 
+public class DeMemoire extends De{
+    //**  ATTRIBUTS   **//
+    private int lancer_precedent;
+
+    //** CONSTRUCTEURS **/
     public DeMemoire(int nbFaces , String nom){
-        super();
-        this.nbFaces=nbFaces;
-        this.nom=nom;
+        super(nbFaces,nom);
         this.lancer_precedent=0;
     }
 
+    //**   METHODES     **/
     @Override
     public int lancer(int nbCoups) {
+        /* fonction qui lance le Dé plusieurs fois (nbCoups de fois)
+        *@param nbCoups = le nombre de fois qu'on lance le Dé
+        **/
         int max=0;
         for (int i=0;i<nbCoups;i++){
             int lancer = lancer();
             while (lancer==lancer_precedent){
-             lancer = lancer();}
-
-            System.out.println(lancer + " le lancer précédent"+ lancer_precedent);
+                lancer = lancer();}
+            //System.out.println(lancer + " le lancer précédent"+ lancer_precedent);
             if(max<lancer){
-                max=lancer;
-            }
-            lancer_precedent=lancer;
-        }
-        return(max);
-    }
+                max=lancer;}
+            lancer_precedent=lancer;}
+        return(max);}
 }
