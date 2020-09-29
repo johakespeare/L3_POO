d2fea6aac30640e56d086bc291d858ec8f397204
Johanna Fericean <71076130+johakespeare@users.noreply.github.com>
2020-09-29 18:04:36 +0200

    Update DePipe.java

--- atelier2/src/DePipe.java
+++ atelier2/src/DePipe.java
@@ -1,27 +1,40 @@
+/**
+ * Classe DePipe
+ * classe fille de la classe De
+ * @version 1.0
+ *
+ * @author Johanna Fericean
+ */
+
 public class DePipe extends De {
-private int borne;
+    //** ATTRIBUTS **//
+    private int borne;
 
-
+    //** CONSTRUCTEURS **/
     public DePipe(int nbFaces , String nom, int borne){
-        super();
-        this.nbFaces=nbFaces;
-        this.nom=nom;
-        if((borne <= nbFaces )&&(borne !=0)){
-        this.borne=borne;}
-        i++;
-
+        /**
+        @param borne = le nombre minimum qui sera tiré au lancer de Dé
+        **/
+        super(nbFaces,nom);
+        if((borne <= nbFaces )&&(borne >0)){  //si la borne est différente de 0 et qu'elle est plus  petite que nbFaces
+        this.borne=borne;
+        }
     }
 
+    //**  METHODES  **//
     public  int getBorne(){
+        /**
+        @return la borne du Dé
+        **/
         return(this.borne);
     }
 
     @Override
     public int lancer() {
-        i =r.nextInt(this.getNbFaces() +1);
-        while ( i <= this.borne) {
-            i =r.nextInt(this.getNbFaces() +1);
+        int tirage =r.nextInt(this.getNbFaces()+1);
+        while ( tirage <= this.borne) {
+            tirage =r.nextInt(this.getNbFaces()+1);
         }
-        return (i);
+        return (tirage);
     }
 }
