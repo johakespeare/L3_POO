5f2cc3c89f7d39e237a8656a0434bdd798005b1c
Johanna Fericean <71076130+johakespeare@users.noreply.github.com>
2020-10-12 20:35:12 +0200

    Update Forme.java

--- Atelier4/src/Forme.java
+++ Atelier4/src/Forme.java
@@ -1,15 +1,19 @@
 public abstract class Forme {
+    //* ATTRIBUTS */
     protected String nom;
     protected static int _n=0;
 
+    //* CONSTRUCTEUR */
     public Forme(String nom){
         /**
          * constructeur de Forme
+         *@param nom, le nom de la Forme
          */
         this.nom=nom;
         _n++;
     }
 
+    //* METHODES */
     public abstract double surface();
 
     public boolean estPlusGrande(Forme forme){
