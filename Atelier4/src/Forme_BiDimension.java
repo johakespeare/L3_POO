5b73f5d5b4a341ba03e88457571d2fa8ff33410d
Johanna Fericean <71076130+johakespeare@users.noreply.github.com>
2020-10-12 20:36:29 +0200

    Update Forme_BiDimension.java

--- Atelier4/src/Forme_BiDimension.java
+++ Atelier4/src/Forme_BiDimension.java
@@ -1,8 +1,12 @@
 public abstract class Forme_BiDimension extends Forme{
-
+ 
+    //*CONSTRUCTEURS*/
     public Forme_BiDimension(String nom) {
+        //*@param nom , le nom de la forme
+        */
         super(nom);
     }
+    //*METHODES*/
     public abstract double perimetre();
 
 
