820a394e4380b31f2de1fb5c7424f26e1fd500fa
Johanna Fericean <71076130+johakespeare@users.noreply.github.com>
2020-10-12 20:37:15 +0200

    Update Forme_TriDimension.java

--- Atelier4/src/Forme_TriDimension.java
+++ Atelier4/src/Forme_TriDimension.java
@@ -1,8 +1,12 @@
 public abstract class Forme_TriDimension extends Forme {
+    //* CONSTRUCTEURS */
     public Forme_TriDimension(String nom) {
+         //*@param nom , le nom de la forme
+        */
         super(nom);
     }
 
+    //*METHODES */
     public abstract double volume();
 
 }
